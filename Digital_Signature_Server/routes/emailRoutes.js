6274b4446f835726c8cdcd6bde4fca51c3f53783
Muhammad Alkhateeb <mhammadalkhateeb22@gmail.com>
2024-08-16 03:11:27 +0300

    solve conflicts

--- Digital_Signature_Server/routes/emailRoutes.js
+++ Digital_Signature_Server/routes/emailRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const emailController = require("../controllers/emailController");
 const signController = require("../controllers/digitalSigningController");
 
-router.post("/email/sendVerify", auth, emailController.sendVerificationEmail);
+router.post("/email/sendVerify", emailController.sendVerificationEmail);
 
-router.post("/email/sendEmail", auth, emailController.sendSigningEmail);
+router.post("/email/sendEmail", emailController.sendSigningEmail);
 
-router.post("/email/verify", auth, emailController.verify);
+router.post("/email/verify", emailController.verify);
 
 router.post("/encryptionRSA", signController.encryptionRSA);
 
